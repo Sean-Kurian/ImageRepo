bd261d1c71f041a75fd53d9e30407debda4ec253
Sean-Kurian <Sean-Kurian@users.noreply.github.com>
2021-09-21 19:20:29 -0400

    initial commit

---
+++ config/users/tests.py
@@ -0,0 +1,10 @@
+import unittest
+from django.test import TestCase
+
+# Create your tests here.
+class APITests2(unittest.TestCase): 
+    def test_1(self): 
+        self.assertEqual(6, 6)
+        self.assertEqual(6, 6)
+        self.assertEqual(6, 6)
+        self.assertEqual(6, 6)
